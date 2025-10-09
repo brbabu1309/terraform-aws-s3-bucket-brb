4e2ce98d9d00fdd3a90e1516c664fc633b3a8154
TEK Student <TEKstudent@vlab.lan>
2025-10-09 11:06:52 -0700

    Updating output variable name

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "bucket_arn" {
   description = "The ARN of the S3 bucket."
   value       = aws_s3_bucket.this.arn
 } 
+
+output "bucket_name" {
+    description ="bucket name to be provided"
+    value = aws_s3_bucket.this.bucket_name
+}
